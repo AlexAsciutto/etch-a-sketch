5c006c4aa7ee0666ea712efebd6a19e68a959b54
“AlexAsciutto” <alexanderasciutto@gmail.com>
2023-04-13 13:36:23 -0400

    Add event listener to button variable that prompts user to input number

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Get the container element
     const container = document.querySelector('#container');
+    const button = document.querySelector('#button');
     let isMouseDown = false;
-    
-    // Create a loop to generate 16 rows
+
+    button.addEventListener('click', () => {
+      const input = prompt('Enter the number of squares per side (max 100)');
+      // rest of the code goes here
+  });  
+
     for (let i = 0; i < 16; i++) {
-      // Create a new row element
       const row = document.createElement('div');
       row.classList.add('row');
   
-      // Create a loop to generate 16 cells for each row
       for (let j = 0; j < 16; j++) {
-        // Create a new cell element
         const cell = document.createElement('div');
         cell.classList.add('cell');
 
-      //
       cell.addEventListener('mousedown', () => {
         isMouseDown = true;
         cell.style.backgroundColor = 'black';
       });
 
-      //
       cell.addEventListener('mouseup', () => {
         isMouseDown = false;
       });
 
-      //
       cell.addEventListener('mousemove', () => {
         if (isMouseDown) {
           cell.style.backgroundColor = 'black';
         }
       });
   
-      // Add the cell to the row
         row.appendChild(cell);
       }
   
-      // Add the row to the container
       container.appendChild(row);
     }
   });
